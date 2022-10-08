4e3fad762195efba1e85c73401d08f11df16e282
acescudero <110642373+acescudero@users.noreply.github.com>
2022-10-07 20:06:06 -0700

    Add files via upload

---
+++ HW1/test_cases/sample_2.c
@@ -0,0 +1,4 @@
+char * str = "a little harder; ";
+/* note how the string literal 
+     above contains a ;
+*/
---
+++ HW1/test_cases/sample_3.c
@@ -0,0 +1,6 @@
+// this example uses the style
+// of comments that start
+// with two slashes “//” and end
+// at the end of the line
+// semicolons (;;;;;) in here don’t
+// count either
